a3982bc41114769970b2c0a8ddcd9cf9efbaf807
siddhantgupta547 <sidgpt84@gmail.com>
2021-09-29 13:10:56 +0530

    Added Actions and Reducers for Profile Component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { fetchUserProfile } from "../actions/profile";
+
+class Profile extends Component {
+  componentDidMount() {
+    const { match } = this.props;
+
+    if (match.params.user_id) {
+      // dispatch an action
+      console.log("mounted");
+      this.props.dispatch(fetchUserProfile(match.params.user_id));
+    }
+  }
+
+  render() {
+    const {
+      match: { params },
+      profile,
+    } = this.props;
+    console.log("this.props", params);
+    const user = profile.user;
+
+    if (profile.inProgress) {
+      return <h1>Loading!</h1>;
+    }
+
+    return (
+      <div className="settings">
+        <div className="img-container">
+          <img
+            src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+            alt="user-dp"
+          />
+        </div>
+
+        <div className="field">
+          <div className="field-label">Name</div>
+          <div className="field-value">{user.name}</div>
+        </div>
+
+        <div className="field">
+          <div className="field-label">Email</div>
+          <div className="field-value">{user.email}</div>
+        </div>
+
+        <div className="btn-grp">
+          <button className="button save-btn">Add Friend</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ profile }) {
+  return {
+    profile,
+  };
+}
+export default connect(mapStateToProps)(Profile);
